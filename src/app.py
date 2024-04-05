94a261feb748c1a219afba3ae6f0957f16610831
Sharon Voon <svoon12@gmail.com>
2024-04-05 16:36:11 -0700

    Updated the new layout for app.py.

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
 import pandas as pd
 from dash import Dash, dcc, callback, Output, Input, html
+import plotly.express as px
 
 
 # Initiatlize the app
@@ -36,35 +37,42 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dbc.Row([
-            ]),
-            dbc.Row([
-            ]),
-        ], md=3
-        ),
-        dbc.Col([
-            # Map chart plot
-        ], md=3
-        ),
-        dbc.Col([
-            dbc.Row([
-                # Line chart plot
-            ]),
-            dbc.Row([
                 dbc.Col([
-                    # Pie chart plot
+                    html.H3(["Crime Type Filter"
+                    ])
                 ]),
                 dbc.Col([
-                    # Stacked bar chart plot
+                    html.H3(["Neighborhood Filter"
+                    ])
                 ]),
             ]),
-        ], md=6
+            dbc.Row([
+                html.H2(["Map Chart"
+                ])
+            ]),
+            dbc.Row([
+                html.H2(["Time Series Chart"
+                ])
+            ])
+        ], md=9
+        ),
+        dbc.Col([
+            dbc.Row([
+                html.H3('Crime Count per Crime Type in {Neighborhood}')
+            ]),
+            dbc.Row([
+                html.H3('Bar Chart')
+            ]),
+            dbc.Row([
+                html.H3('Crime Count per Neighborhood for {CrimeType}')
+            ]),
+            dbc.Row([
+                html.H3('Bar Chart')
+            ])
+        ],md=3
         )
     ])
 ])
-
-
-
-
 
 # Run the app/dashboard
 if __name__ == '__main__':
